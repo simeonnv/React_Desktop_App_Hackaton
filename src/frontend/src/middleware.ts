f941011497f404a54f427b62a61b0db3b3c2ce94
atmnsh <kapusta.elizaveta73@gmail.com>
2025-02-22 08:04:36 +0200

    use good reference

--- src/frontend/src/middleware.ts
+++ src/frontend/src/middleware.ts
@@ -1,60 +1,54 @@
 // src/middleware.ts
-import { defineMiddleware, sequence } from 'astro:middleware';
+import { defineMiddleware, sequence } from "astro:middleware";
 
 import { PUBLIC_ROUTES } from "./const";
 
 const AuthMiddleware = defineMiddleware(async (context, next) => {
+  const token: any =
+    context.request.headers.get("authorization") ||
+    context.cookies.get("auth-token");
 
-    const token: any = context.request.headers.get('authorization') ||
-        context.cookies.get('auth-token');
+  let exists = true;
+  // try {
+  const existsRes = await fetch(`http://hackaton_backend:6004/auth/exists`, {
+    method: "GET",
+  });
+  console.log("existsRes ", existsRes);
+  if (existsRes.ok) {
+    exists = (await existsRes.json()).data;
+  }
+  // } catch {}
 
-    let exists = true;
-    // try {
-        const existsRes = await fetch(`http://localhost:6004/auth/exists`, {method: "GET"});
-        console.log("existsRes ", existsRes)
-        if (existsRes.ok) {
-            exists = (await existsRes.json()).data
-        }
-    // } catch {}
+  console.log(exists);
 
-    console.log(exists);
-
-    if (!token) {
-        if (PUBLIC_ROUTES.includes(context.url.pathname)) {
-            return next();
-        }
-
-        if (exists)
-            return Response.redirect(new URL('/login', context.url));
-        else
-            return Response.redirect(new URL('/signup', context.url));
+  if (!token) {
+    if (PUBLIC_ROUTES.includes(context.url.pathname)) {
+      return next();
     }
 
+    if (exists) return Response.redirect(new URL("/login", context.url));
+    else return Response.redirect(new URL("/signup", context.url));
+  }
 
-    const validateRes = await fetch(
-        `http://localhost:6004/auth/validate`, // Use environment variable
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token.value}`
-            }
-        }
-    );
-
-
-
-    if (validateRes.ok) {
-        const validateResult = await validateRes.json();
-        context.locals.user = validateResult.data;
-        return next();
-    } else {
-        if (exists)
-            return Response.redirect(new URL('/login', context.url));
-        else
-            return Response.redirect(new URL('/signup', context.url));
+  const validateRes = await fetch(
+    `http://hackaton_backend:6004/auth/validate`, // Use environment variable
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.value}`,
+      },
     }
+  );
+
+  if (validateRes.ok) {
+    const validateResult = await validateRes.json();
+    context.locals.user = validateResult.data;
+    return next();
+  } else {
+    if (exists) return Response.redirect(new URL("/login", context.url));
+    else return Response.redirect(new URL("/signup", context.url));
+  }
 });
-
 
 export const onRequest = sequence(AuthMiddleware);
