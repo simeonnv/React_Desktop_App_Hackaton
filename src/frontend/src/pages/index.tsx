dc23467691d1748627f9b726307a2a71818915ad
atmnsh <kapusta.elizaveta73@gmail.com>
2025-02-22 21:31:49 +0200

    sidebar

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { FloatingDock, links } from "@/components/ui/floating-dock";
 import { CardDemo } from "@/components/auth/CardDemo";
+import { SidebarDemo } from "@/components/auth/SidebarDemo";
 
 export default function CombinedComponent() {
   return (
     <>
-      <div className="min-h-screen w-full bg-blue-500 flex items-center justify-center">
+      <div className="min-h-screen w-full bg-blue-500 flex flex-col items-center justify-center">
+        <SidebarDemo />
         <FloatingDock mobileClassName="translate-y-20" items={links} />
         <CardDemo />
       </div>
