d8e4ecef291a03f2b17cb04c5cdad861250b8ac1
simeonnv2 <111970903+simeonnv@users.noreply.github.com>
2025-02-22 18:03:06 +0200

    usage update

--- src/backend/src/routes/docker/socket_docker_usage.rs
+++ src/backend/src/routes/docker/socket_docker_usage.rs
@@ -87,27 +87,7 @@ struct SocketDockerUsageResDocs {
     responses(
         (status = 101, description = "WebSocket connection established", body = SocketDockerUsageResDocs, example = json!({
             "status": "success",
-            "data": [
-                {
-                "read": "2025-02-22T12:28:07.770076583Z",
-                "names": [
-                  "/react_desktop_app_hackaton-hackaton_backend-1"
-                ],
-                "id": "c377b1e1bde7712280fd757235796736d09d5e1bd333bbe4198d1d5ba686152d",
-                "memory_stats": {
-                  "usage": 15470592,
-                  "limit": 334621286
-                },
-                "cpu_stats": {
-                  "online_cpus": 16,
-                  "total_usage": 554300000
-                },
-                "pids_stats": {
-                  "current": 18,
-                  "limit": 38202
-                }
-              },
-            ]
+            "data": [{"read":"2025-02-22T16:01:49.99635365Z","names":["/react_desktop_app_hackaton-hackaton_frontend-1"],"id":"6bede12b8d3146ee50cd30572ad9f85ec984881bb72dcb30546671602bde1660","memory_stats":{"usage":246980608,"limit":3328640},"cpu_stats":{"online_cpus":16,"total_usage":1418034000},"pids_stats":{"current":85,"limit":38202},"network":{"rx_dropped":0,"rx_bytes":11274,"rx_errors":0,"tx_packets":18,"tx_dropped":0,"rx_packets":36,"tx_errors":0,"tx_bytes":4407}},{"read":"2025-02-22T16:01:49.996334504Z","names":["/react_desktop_app_hackaton-hackaton_backend-1"],"id":"9cc7596fde34ce2e6a517568972c4e340938ed51d472094e0396cf873aca178b","memory_stats":{"usage":16330752,"limit":33640},"cpu_stats":{"online_cpus":16,"total_usage":58720000},"pids_stats":{"current":19,"limit":38202},"network":{"rx_dropped":0,"rx_bytes":12633,"rx_errors":0,"tx_packets":98,"tx_dropped":0,"rx_packets":97,"tx_errors":0,"tx_bytes":11559}},{"read":"2025-02-22T16:01:49.996334213Z","names":["/react_desktop_app_hackaton-hackaton_database-1"],"id":"ce479d96c38b1d671c629ae20946d9b51258a42f75282df7728aa43550f1a007","memory_stats":{"usage":30339072,"limit":33428640},"cpu_stats":{"online_cpus":16,"total_usage":153034000},"pids_stats":{"current":8,"limit":38202},"network":{"rx_dropped":0,"rx_bytes":13889,"rx_errors":0,"tx_packets":74,"tx_dropped":0,"rx_packets":115,"tx_errors":0,"tx_bytes":9262}}]
         })),
         (status = 400, description = "Bad request - WebSocket upgrade failed")
     ),
