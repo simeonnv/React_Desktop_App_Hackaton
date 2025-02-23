852c3ae77d532ca2c5a34d147d88c2d912a7ec70
atmnsh <kapusta.elizaveta73@gmail.com>
2025-02-23 10:03:33 +0200

    Searchbar visual

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { FloatingDock, links } from "@/components/ui/floating-dock";
 import { Card } from "@/components/auth/Card";
 import { Sidebar } from "@/components/auth/Sidebar";
+import Searchbar from "@/components/auth/Searchbar";
 
 export default function CombinedComponent() {
   return (
     <>
       <div className="min-h-screen w-full bg-blue-500 flex flex-col items-center justify-center">
+        <Searchbar />
         <Sidebar />
-        <FloatingDock mobileClassName="translate-y-20" items={links} />
+
         <Card />
       </div>
     </>
